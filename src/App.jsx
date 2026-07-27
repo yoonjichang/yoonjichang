d8bb386cce01a8f1c0f64373b391d992f00cbdb7
yoonjichang <wlckd2003@gmail.com>
2026-07-28 01:02:25 +0900

    Vercel 주소 인식 오류 해결을 위한 HashRouter 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Routine from './pages/Routine';
 import WorkoutSession from './pages/WorkoutSession';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
+        {/* 정상적인 주소들 */}
         <Route path="/" element={<Routine />} />
         <Route path="/routine" element={<Routine />} />
         <Route path="/workout" element={<WorkoutSession />} />
+        
+        {/* 🚨 주소를 찾지 못해 길을 잃으면 무조건 홈 화면(Routine)으로 강제 이동! */}
+        <Route path="*" element={<Routine />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
